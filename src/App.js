d22ba25420a4b07e1d087b33f7192e700585d0b8
Igor <igorperic996@gmail.com>
2024-02-14 22:17:26 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Post from './components/Post';
+
+
+function App() {
+  return (<main>
+    <Post author="Maximilian" body="React.js is awesome!"/>
+    <Post author="Manuel" body="Check out the full course!"/>
+    <Post />
+    <Post />
+  </main>);
+}
+
+export default App;
