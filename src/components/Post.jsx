4876d8580e60f87e90b9287cd8c3644513c4239a
Igor <igorperic996@gmail.com>
2024-02-16 12:29:05 +0100

    added-form-for-new-post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,9 @@
 import classes from './Post.module.css';
 
 function Post(props) {
-    return <div className={classes.post}>
+    return <li className={classes.post}>
         <p className={classes.author}>{props.author}</p>
         <p className={classes.text}>{props.body}</p>
-    </div>
+    </li>
 }
 export default Post;
